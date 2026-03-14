399927b10b127872d77ff0d9b0b7f32657bbe253
Adam Abdallah <your@email.com>
2026-03-14 12:43:44 +0200

    Adding base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import { base } from "framer-motion/client"
 
 export default defineConfig({
+  base: '/tools/',
   plugins: [
     react(),
     tailwindcss()
