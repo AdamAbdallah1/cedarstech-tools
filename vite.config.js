32e2a08501963bffaa09972762304459e0f215ff
Adam Abdallah <your@email.com>
2026-03-14 12:57:43 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
-import { base } from "framer-motion/client"
 
 export default defineConfig({
   base: '/tools/',
   plugins: [
     react(),
     tailwindcss()
-  ]
+  ],
+  build: {
+    outDir: 'dist/tools',
+    emptyOutDir: true
+  }
 })
